1a9b1cbef54c8da83e0be5fa82e93debed4a7e51
K <61808204+killind-dev@users.noreply.github.com>
2023-08-08 23:15:43 -0700

    documentation

--- langtree/core/operator.py
+++ langtree/core/operator.py
@@ -1,45 +1,96 @@
+
 import backoff
 
-
 def default_call(*args, **kwargs):
+    """Default call function that returns the provided keyword arguments.
+
+    Args:
+        *args: Variable length argument list.
+        **kwargs: Arbitrary keyword arguments.
+
+    Returns:
+        dict: The provided keyword arguments.
+    """
     return kwargs
 
-
 def default_parse(output):
+    """Default parse function that returns the provided output without modifications.
+
+    Args:
+        output: The output to parse.
+
+    Returns:
+        The unmodified output.
+    """
     return output
 
-
 def freeze(function, **top_kwargs):
+    """Return a function with specific arguments frozen.
+
+    Args:
+        function (callable): The function to freeze arguments for.
+        **top_kwargs: Keyword arguments to freeze.
+
+    Returns:
+        callable: The function with specific arguments frozen.
+    """
     def frozen(*args, **kwargs):
         return function(*args, **{**top_kwargs, **kwargs})
-
     return frozen
 
-
 def chainable(func):
+    """Decorator that makes a function chainable with its arguments.
+
+    Args:
+        func (callable): The function to make chainable.
+
+    Returns:
+        callable: The chainable function.
+    """
     def wrapper(**kws):
         return freeze(func, **kws)
-
     return wrapper
 
-
 class Operator(object):
+    """A class to represent and process custom call and parse operations."""
 
     def __init__(self, call=default_call, parse=default_parse):
+        """Initialize the Operator with custom call and parse functions.
+
+        Args:
+            call (callable, optional): The custom call function. Defaults to default_call.
+            parse (callable, optional): The custom parse function. Defaults to default_parse.
+        """
         self.call = call
         self.parse = parse
 
-    # @backoff.on_exception(backoff.expo(2, 2), Exception, max_tries=5)
     def __call__(self, *args, **kwargs):
-        res = self.call(*args, **kwargs)
+        """Call the Operator's call function and parse its result.
 
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            The parsed result of the call function.
+        """
+        res = self.call(*args, **kwargs)
         if self.parse is not None:
             res = self.parse(res)
-
         return res
 
     def freeze_call(self, **kwargs):
+        """Freeze specific arguments for the Operator's call function.
+
+        Args:
+            **kwargs: Keyword arguments to freeze.
+        """
         self.call = freeze(self.call, **kwargs)
 
     def freeze_parse(self, **kwargs):
+        """Freeze specific arguments for the Operator's parse function.
+
+        Args:
+            **kwargs: Keyword arguments to freeze.
+        """
         self.parse = freeze(self.parse, **kwargs)
--- langtree/prompting/message_types.py
+++ langtree/prompting/message_types.py
@@ -1,7 +1,29 @@
+
 from langtree.utils.data import Data
 
-ChatMessage = Data(role=None, content=None)
-SystemMessage = Data(role="system", content=None)
-AssistantMessage = Data(role="assistant", content=None)
-UserMessage = Data(role="user", content=None)
-FunctionMessage = Data(role="function", content=None, name=None)
+class ChatMessage(Data):
+    """A generic chat message with attributes role and content."""
+    role = None
+    content = None
+
+class SystemMessage(Data):
+    """A system message with predefined role as "system" and an attribute content."""
+    role = "system"
+    content = None
+
+class AssistantMessage(Data):
+    """An assistant message with predefined role as "assistant" and an attribute content."""
+    role = "assistant"
+    content = None
+
+class UserMessage(Data):
+    """A user message with predefined role as "user" and an attribute content."""
+    role = "user"
+    content = None
+
+class FunctionMessage(Data):
+    """A function message with attributes role, content, and name. 
+    The role is predefined as "function"."""
+    role = "function"
+    content = None
+    name = None
